bc323db4ea53539ce0988411ca1d315a7ba0e4e8
André Porto <andreporto@me.com>
2023-11-01 06:54:55 -0300

    Initial Commit

---
+++ MyTour/MyTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTourApp.swift
+//  MyTour
+//
+//  Created by André Porto on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MyTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
